91abd87ddaeac1fb9c5a91a0091281c9bb66d93a
Konstantin Intsyn <intsyn@mail.ru>
2022-12-15 22:14:15 +0300

    fix redis setting

--- app/settings/inc_django/cache.py
+++ app/settings/inc_django/cache.py
@@ -3,15 +3,15 @@ from settings.base import env
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.redis.RedisCache',
-        'LOCATION': env.str('REDIS_CACHE', default='redis://127.0.0.1:6379/0'),
+        'LOCATION': env.str('REDIS_CACHE_DEFAULT', default='redis://127.0.0.1:6379/0'),
     },
     'repeats': {
         'BACKEND': 'django.core.cache.backends.redis.RedisCache',
-        'LOCATION': env.str('REDIS_CACHE', default='redis://127.0.0.1:6379/1'),
+        'LOCATION': env.str('REDIS_CACHE_REPEATS', default='redis://127.0.0.1:6379/1'),
     },
     'file_uploading': {
         'BACKEND': 'django.core.cache.backends.redis.RedisCache',
-        'LOCATION': env.str('REDIS_CACHE', default='redis://127.0.0.1:6379/2'),
+        'LOCATION': env.str('REDIS_CACHE_FILE_UPLOADING', default='redis://127.0.0.1:6379/2'),
     }
 }
 
