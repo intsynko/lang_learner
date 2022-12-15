3d4e59978959a8cdd6ee8636d9dd0b91c06be460
Konstantin Intsyn <intsyn@mail.ru>
2022-12-15 20:36:47 +0300

    add deploy settings

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM python:3.10
+
+## install dependencies for psycopg2
+RUN apt-get update \
+    && apt-get -y install libpq-dev gcc \
+    && pip install psycopg2
+
+RUN mkdir /app
+WORKDIR /app
+RUN pip install poetry
+COPY poetry.lock pyproject.toml /app/
+ENV DJANGO_SETTINGS_MODULE=settings.production
+#RUN poetry install
+RUN poetry config virtualenvs.create false \
+  && poetry install --no-interaction --no-ansi
+COPY app /app/
+RUN python manage.py collectstatic
--- app/urls.py
+++ app/urls.py
@@ -1,16 +1,19 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("apps.web.urls", namespace="web")),
-    path("api/", include("api.urls"))
+    path("api/", include("api.urls")),
+
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})
 ]
 
 urlpatterns += static(settings.ASSETS_URL, document_root=settings.ASSETS_ROOT)
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
